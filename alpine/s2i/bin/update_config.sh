576e148bcc9a5f1609429694546fa6b361591147
Amon-Ra <amon.raj@gmail.com>
2019-03-12 12:15:50 +0100

    fix update_config

--- alpine/s2i/bin/update_config.sh
+++ alpine/s2i/bin/update_config.sh
@@ -3,8 +3,8 @@ NGINX_CONF_PATH=${NGINX_CONF_PATH:-/opt/app-root/etc/nginx.conf}
 
 while true
 do
-    inotifywait -m -r /etc/nginx/conf.d
+    inotifywait -e CREATE -r /etc/nginx/conf.d
+    sleep 4
     conf-nginx
     nginx -s reload -c $NGINX_CONF_PATH
-    sleep 4
 done
