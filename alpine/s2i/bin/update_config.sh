003212674ada467dd62d53c302ad8bcc5a0c194f
Amon-Ra <amon.raj@gmail.com>
2019-03-12 10:51:02 +0100

    fix reload

--- alpine/s2i/bin/update_config.sh
+++ alpine/s2i/bin/update_config.sh
@@ -1,7 +1,10 @@
 #!/bin/dash
+NGINX_CONF_PATH=${NGINX_CONF_PATH:-/opt/app-root/etc/nginx.conf}
+
 while true
 do
     inotifywait -m -r /etc/nginx/conf.d
     conf-nginx
+    nginx -s reload -c $NGINX_CONF_PATH
     sleep 4
 done
