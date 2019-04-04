035fd2b1d4a9dcf10902be8ef25e09e27653c14d
Amon-Ra <amon.raj@gmail.com>
2019-04-04 09:16:45 +0200

    Add pem generator for cert-manager

--- alpine/s2i/bin/update_config.sh
+++ alpine/s2i/bin/update_config.sh
@@ -1,10 +1,10 @@
 #!/bin/dash
-NGINX_CONF_PATH=${NGINX_CONF_PATH:-/opt/app-root/etc/nginx.conf}
+NGINX_CONF_PATH=${NGINX_CONF_PATH:-/opt/app-root/etc}
 
 while true
 do
-    inotifywait -e CREATE -r /etc/nginx/conf.d
+    inotifywait -e CREATE -r $NGINX_CONF_PATH
     sleep 4
     conf-nginx
-    nginx -s reload -c $NGINX_CONF_PATH
+    nginx -s reload -c $NGINX_CONF_PATH/nginx.conf
 done
