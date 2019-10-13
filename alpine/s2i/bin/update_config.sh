124570b8ebc8336572afd597793f0b7f8b1f3b5a
Amon-Ra <amon.raj@gmail.com>
2019-10-13 21:18:21 +0200

    fix

--- alpine/s2i/bin/update_config.sh
+++ alpine/s2i/bin/update_config.sh
@@ -1,16 +1,10 @@
 #!/bin/dash
 NGINX_CONF_PATH=${NGINX_CONF_PATH:-/opt/app-root/etc}
-NGINX_CONF_WATCH_PATH=${NGINX_CONF_WATCH_PATH:-''}
-WATCH_STR="-r $NGINX_CONF_PATH"
+NGINX_CONF_WATCH_PATH="${NGINX_CONF_WATCH_PATH} ${NGINX_CONF_PATH}"
 
-for i in $(echo $NGINX_CONF_WATCH_PATH | tr ',' ' ')
-do
-    WATCH_STR="$WATCH_STR -r \"$i\""
-done
-echo $WATCH_STR
 while true
 do
-    inotifywait -e CREATE $WATCH_STR
+    inotifywait -e CREATE -r $NGINX_CONF_WATCH_PATH
     sleep 4
     conf-nginx
     nginx -s reload -c $NGINX_CONF_PATH/nginx.conf
